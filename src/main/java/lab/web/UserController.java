87185754fbad9676971cd77fe766d328b399f75b
Drago <88schweppes888@gmail.com>
2022-06-12 11:26:28 +0300

    adding some server side validations.

--- src/main/java/lab/web/UserController.java
+++ src/main/java/lab/web/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
@@ -46,7 +48,7 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(UserRegisterDTO userRegisterDTO) {
+    public String register(@Valid UserRegisterDTO userRegisterDTO) {
         userService.registerAndLogin(userRegisterDTO);
         return "redirect:/";
     }
